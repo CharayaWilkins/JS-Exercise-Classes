74c40a059991633634c9edd441f11d86803af844
CharayaWilkins <rayawilkins@gmail.com>
2021-07-22 16:55:39 -0700

    finished last task

--- index.js
+++ index.js
@@ -81,11 +81,26 @@ const kenny = new Person({
 */
 
 class Car {
-  constructor(model, milesPerGallon) {
+  constructor(model, mpg) {
     this.model = model;
-    this.milesPerGallon = milesPerGallon;
+    this.milesPerGallon = mpg;
+    this.tank = 0;
+    this.odometer = 0
   }
-  
+  fill(gallons){
+    this.tank = this.tank + gallons;
+  }
+  drive(distance){
+    const driveableMiles = this.tank * this.milesPerGallon;
+    if(distance <= driveableMiles){
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - (distance / this.milesPerGallon);
+    }else{
+      this.odometer = this.odometer + driveableMiles;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer}`;
+    }
+  }
 }
 
 /*

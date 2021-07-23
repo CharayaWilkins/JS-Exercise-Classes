53bb64f6697f8f928ef4e73f915cdf2b14a79b24
CharayaWilkins <rayawilkins@gmail.com>
2021-07-22 17:13:03 -0700

    studying syntax

--- index.js
+++ index.js
@@ -116,10 +116,10 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-  constructor(info) {
-    this.name = info.name,
-    this.age = info.age,
-    this.location = info.location
+  constructor({name, age, location}) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
   }
   speak() {
     return `Hello my name is ${this.name}, I am from ${this.location}`;
@@ -141,11 +141,11 @@ class Lambdasian {
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 class Instructor extends Lambdasian {
-  constructor(info) {
-    super(info);
-    this.specialty = info.specialty,
-    this.favLanguage = info.favLanguage,
-    this.catchPhrase = info.catchPhrase
+  constructor({name, age, location, specialty, favLanguage, catchPhrase}){
+    super({name, age, location, specialty, favLanguage, catchPhrase});
+    this.specialty = specialty;
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase;
   }
   demo(subject) {
     return `Today we are learning about ${subject}`;
@@ -170,21 +170,20 @@ class Instructor extends Lambdasian {
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
 class Student extends Lambdasian {
-  constructor(info) {
-    super(info);
-    this.previousBackground = info.previousBackground,
-    this.className = info.className,
-    this.favSubjects = []
+  constructor({name, age, location, previousBackground, className, favSubjects}) {
+    super({name, age, location, previousBackground, className, favSubjects});
+    this.previousBackground = previousBackground;
+    this.className = className;
+    this.favSubjects = favSubjects;
   }
   listSubjects(){
-    this.favSubjects.push('Loving HTML, CSS, JS!');
-    return this.favSubjects;
+    return `Loving ${this.favSubjects}`;
   }
   PRAssignment(subject) {
-    return `${student.name} has submitted a PR for ${subject}`;
+    return `${this.name} has submitted a PR for ${subject}`;
   }
   sprintChallenge(subject) {
-    return `${student.name} has begun sprint challenge on ${subject}`;
+    return `${this.name} has begun sprint challenge on ${subject}`;
   }
 }
 
@@ -202,10 +201,10 @@ class Student extends Lambdasian {
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 class ProjectManager extends Instructor{
-  constructor(info) {
-    super(info);
-    this.gradClassName = info.gradClassName,
-    this.favInstructor = info.favInstructor
+  constructor({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor}) {
+    super({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor});
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
   }
    standup(channel) {
      return `${this.name} announces to ${channel}, @channel standy times!`; 
